90f11df16f03883c6a563d5aa8ba8f43ee4d0d40
claytonzhuo <eafanzhuo@foxmail.com>
2026-04-02 23:59:08 +0800

    更新useDebouncedValue

--- src/hooks/useDebouncedValue.ts
+++ src/hooks/useDebouncedValue.ts
@@ -4,7 +4,7 @@ import { useEffect, useState, useRef } from "react";
  * 对输入值做防抖，延迟后才返回最新值。
  * @param value 需要防抖的任意类型值
  * @param delay 防抖延迟时间（毫秒）
- * @returns 防抖后的value
+ * @returns 防抖后的值
  */
 export default function useDebouncedValue<T>(value: T, delay: number) {
   const [debouncedvalue, setDebouncedValue] = useState(value);
