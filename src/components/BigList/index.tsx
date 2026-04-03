bf6ca8d125c16bc68ed6dfc4f80168cda40c2dfc
claytonzhuo <eafanzhuo@foxmail.com>
2026-04-03 15:15:15 +0800

    修改CLAUDE.md

--- src/components/BigList/index.tsx
+++ src/components/BigList/index.tsx
@@ -1,10 +1,11 @@
-import ExpensiveItem from "./TransitionTest/ExpensiveItem";
+import ExpensiveItem from "../TransitionTest/ExpensiveItem";
+import styles from "./style.module.scss";
 
 export default function BigList({ query }: { query: string }) {
     const items = Array.from({ length: 1000 }, (_, i) => i);
 
     return (
-        <ul>
+        <ul className={styles.list}>
             {items.map(i => (
                 <ExpensiveItem key={i + query} index={i} query={query} />
             ))}
